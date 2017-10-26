8b965985225f11c87a339775582941e4c2278219
Playspal <playspal@gmail.com>
2017-10-26 10:47:30 +0300

    Entry point added

---
+++ UnityExpansion.cs
@@ -0,0 +1,22 @@
+﻿using UnityExpansion.Services;
+
+namespace UnityExpansion
+{
+    public static class UnityExpansion
+    {
+        public const string SIGNAL_FRAME_START = "UnityExpansion/FrameStart";
+
+        /// <summary>
+        /// Update internal part of UnityExpansion
+        /// </summary>
+        public static void Update()
+        {
+            Singnals.Dispatch(SIGNAL_FRAME_START);
+        }
+
+        public static void ExecuteInMainThread()
+        {
+
+        }
+    }
+}
