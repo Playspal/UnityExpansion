786c01eba012d25cd795e43e67adf50fb49aa1cc
Playspal <playspal@gmail.com>
2017-12-03 22:53:55 +0300

    Main Expansion object changed

---
+++ Expansion.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityExpansion.Services;
+using UnityExpansion.UI;
+
+namespace UnityExpansion
+{
+    /// <summary>
+    /// Main class of UnityExpansion and it should be added as component to one of game objects.
+    /// </summary>
+    [AddComponentMenu("Expansion/Expansion Main Object", 1)]
+    public class Expansion : MonoBehaviour
+    {
+        /// <summary>
+        /// Signal name that dispatched on start of each frame.
+        /// </summary>
+        public const string SIGNAL_FRAME_START = "UnityExpansion/FrameStart";
+
+        /// <summary>
+        /// Expansion instance.
+        /// </summary>
+        public static Expansion Instance { get; private set; }
+
+        [SerializeField]
+        public UiLayoutSettings LayoutSettings;
+
+        // Initialization
+        private void Awake()
+        {
+            Instance = this;
+        }
+
+        // Updates internal part of UnityExpansion.
+        private void Update()
+        {
+            Signals.Dispatch(SIGNAL_FRAME_START);
+        }
+
+        // Resets UnityExpansion component to default state.
+        private void Reset()
+        {
+            if(LayoutSettings == null)
+            {
+                LayoutSettings = gameObject.GetOrAddComponent<UiLayoutSettings>();
+                LayoutSettings.hideFlags = HideFlags.HideInInspector;
+            }
+        }
+    }
+}
