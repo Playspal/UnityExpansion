86738992b6abc671baf3135ad0a8eb3b1826d616
okovtechround <aokov@fighterbase.net>
2019-02-01 21:14:31 +0300

    WIP commit

---
+++ Internal/InternalWizardMouse.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityExpansionInternal
+{
+    public class InternalWizardMouse
+    {
+        public float X { get; private set; }
+        public float Y { get; private set; }
+
+        public void SetPosition(float x, float y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
---
+++ Internal/UiFlow/InternalUiFlowEditor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace UnityExpansionInternal
+{
+    public class InternalUiFlowEditor : InternalWizard
+    {
+        public static InternalUiFlowEditor Instance { get; private set; }
+
+        protected override void OnWizardCreate()
+        {
+            base.OnWizardCreate();
+            Debug.LogError("!!!222");
+        }
+
+        protected override void OnGUI()
+        {
+            base.OnGUI();
+
+            EditorGUILayout.HelpBox("123", MessageType.Info);
+        }
+
+        /// <summary>
+        /// Shows the window.
+        /// </summary>
+        [MenuItem("Window/UI Flow Editor")]
+        public static void ShowWindow()
+        {
+            Instance = DisplayWizard<InternalUiFlowEditor>("UI Flow Editor", "sdfsdfs", "asdadas");
+        }
+    }
+}
