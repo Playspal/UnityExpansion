a316d7954274520a1d8b93e98bf95476c7b34814
Playspal <playspal@gmail.com>
2019-03-09 11:09:38 +0300

    Layout events added

--- Editor/EditorLayoutMouse.cs
+++ Editor/EditorLayoutMouse.cs
@@ -10,15 +10,6 @@ namespace UnityExpansion.Editor
     /// </summary>
     public class EditorLayoutMouse
     {
-        public enum MouseEvent
-        {
-            Press,
-            Release,
-            Move,
-            Over,
-            Out
-        }
-
         public event Action OnPress;
         public event Action OnRelease;
         public event Action OnMove;
