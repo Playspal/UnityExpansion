6c74b32c2f5202c6bfe5489ec619ee3a1a440935
okovtechround <aokov@fighterbase.net>
2019-03-15 16:48:52 +0300

    Draft object reference filed added to layout

---
+++ Editor/EditorLayoutObjectReference.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace UnityExpansion.Editor
+{
+    public class EditorLayoutObjectReference : EditorLayoutObject
+    {
+        // TODO: finish implementation
+        public EditorLayoutObjectReference(EditorLayout window, int width, int height) : base(window, width, height)
+        {
+        }
+
+        public override void Render()
+        {
+            base.Render();
+            EditorGUI.ObjectField(new Rect(GlobalX, GlobalY, Width, Height), "", null, typeof(GameObject), true);
+        }
+    }
+}
